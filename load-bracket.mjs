d9d32a3c588beb5cf1068c10fa6ea2ef2dfcc4b3
Nick Thurow <nthurow@gmail.com>
2024-03-18 22:11:29 -0500

    get initial fetch working

---
+++ load-bracket.mjs
@@ -0,0 +1,90 @@
+import {load} from 'cheerio';
+
+const bracketPage = await fetch('https://www.espn.com/mens-college-basketball/bracket', {
+  headers: {
+    accept:
+      "text/htmlimport {} from 'pplication/xhtml+xmlimport {} from 'pplication/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8import {} from 'pplication/signed-exchange;v=b3;q=0.7",
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'no-cache',
+    pragma: 'no-cache',
+    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    Referer: 'https://www.espn.com/mens-college-basketball/',
+    'Referrer-Policy': 'origin-when-cross-origin'
+  },
+  body: null,
+  method: 'GET'
+});
+
+const html = await bracketPage.text();
+const root = load(html);
+
+// console.log(html);
+const matchupLinks = root('a.AnchorLink.BracketCell')
+  .toArray()
+  .map((el) => el.attribs.href);
+
+const matchupInfo = await Promise.all(
+  matchupLinks.map(async (matchupLink) => {
+    const matchupContent = await fetch(`https://www.espn.com${matchupLink}`, {
+      headers: {
+        accept:
+          "text/htmlimport {} from 'pplication/xhtml+xmlimport {} from 'pplication/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8import {} from 'pplication/signed-exchange;v=b3;q=0.7",
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        pragma: 'no-cache',
+        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        Referer: 'https://www.espn.com/mens-college-basketball/bracket',
+        'Referrer-Policy': 'strict-origin-when-cross-origin'
+      },
+      body: null,
+      method: 'GET'
+    });
+
+    const matchupHtml = await matchupContent.text();
+    const matchupRoot = load(matchupHtml);
+
+    const teamNameAndRank = matchupRoot('div.Gamestrip__Info')
+      .toArray()
+      .map((el) => {
+        const rank = matchupRoot(el).find('.ScoreCell__Rank').text();
+        const name = matchupRoot(el).find('.ScoreCell__TeamName').text();
+
+        return {
+          rank: +rank,
+          name
+        };
+      });
+
+    const teamSchedule = matchupRoot('div.LastGames__GameList footer.Card__Footer')
+      .toArray()
+      .map((el) => {
+        return matchupRoot(el).find('a.AnchorLink').attr('href');
+      });
+
+    return teamNameAndRank.map((team, index) => {
+      return {...team, schedule: teamSchedule[index]};
+    });
+  })
+);
+
+console.log(matchupInfo);
+/*
+  .map((el) => {
+    return el.attribs['href'];
+  })
+  .forEach(console.log);
+  */
