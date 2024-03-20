d3cc9c00b5d7f2c307f708a0fff022d3525fe14f
Nick Thurow <nthurow@gmail.com>
2024-03-19 19:25:49 -0500

    produce JSON with teams in tournament attached with stats

--- load-bracket.mjs
+++ load-bracket.mjs
@@ -94,117 +94,129 @@ const allTeams = matchupInfo.reduce((soFar, matchup) => {
 }, []);
 
 const allTeamsWithStatus = await Promise.all(
-  allTeams.map(async (team) => {
-    const schedulePage = await fetch(team.schedule, {
-      headers: {
-        accept:
-          "text/htmlimport {} from 'pplication/xhtml+xmlimport {} from 'pplication/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8import {} from 'pplication/signed-exchange;v=b3;q=0.7",
-        'accept-language': 'en-US,en;q=0.9',
-        'cache-control': 'no-cache',
-        pragma: 'no-cache',
-        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1'
-      },
-      body: null,
-      method: 'GET'
-    });
-
-    const scheduleHtml = await schedulePage.text();
-    const scheduleRoot = load(scheduleHtml);
-
-    const resultRows = scheduleRoot('tr')
-      .toArray()
-      .filter((resultRow) => {
-        return scheduleRoot(resultRow).children('td').length === 7;
-      })
-      .filter((resultRow) => {
-        return scheduleRoot(resultRow).children().first().text() !== 'DATE';
+  allTeams
+    .filter((team) => !!team.schedule) // The play-in games are in-progress right now and clicking on them takes you to a different page w/ no schedule
+    .map(async (team) => {
+      const schedulePage = await fetch(team.schedule, {
+        headers: {
+          accept:
+            "text/htmlimport {} from 'pplication/xhtml+xmlimport {} from 'pplication/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8import {} from 'pplication/signed-exchange;v=b3;q=0.7",
+          'accept-language': 'en-US,en;q=0.9',
+          'cache-control': 'no-cache',
+          pragma: 'no-cache',
+          'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Linux"',
+          'sec-fetch-dest': 'document',
+          'sec-fetch-mode': 'navigate',
+          'sec-fetch-site': 'same-origin',
+          'sec-fetch-user': '?1',
+          'upgrade-insecure-requests': '1'
+        },
+        body: null,
+        method: 'GET'
       });
 
-    const scores = resultRows
-      .map((resultRow) => {
-        return {
-          resultCell: scheduleRoot(resultRow).children('td').toArray()[2],
-          opponentCell: scheduleRoot(resultRow).children('td').toArray()[1]
-        };
-      })
-      .map((resultCells) => {
-        const result = scheduleRoot(resultCells.resultCell).text();
-        const isVictory = result.startsWith('W');
-        const scores = result
-          .slice(1)
-          .replace(' OT', '')
-          .split('-')
-          .reduce((soFar, part, index) => {
-            const scoreInt = parseInt(part, 10);
+      const scheduleHtml = await schedulePage.text();
+      const scheduleRoot = load(scheduleHtml);
 
-            if (typeof scoreInt !== 'number') {
-              throw new Error(`The value ${part} is not a number`);
-            }
+      const resultRows = scheduleRoot('tr')
+        .toArray()
+        .filter((resultRow) => {
+          return scheduleRoot(resultRow).children('td').length === 7;
+        })
+        .filter((resultRow) => {
+          return scheduleRoot(resultRow).children().first().text() !== 'DATE';
+        });
 
-            return {...soFar, ...(index === 0 ? {first: scoreInt} : {second: scoreInt})};
-          }, {});
-
-        const opponentLink = scheduleRoot(resultCells.opponentCell).find('a').last();
-        const opponentName = opponentLink.text().trim();
-        const opponentHref = opponentLink.attr('href');
-        const opponentId = opponentHref?.split('/')?.slice(-2, -1).pop();
-
-        const opponentIdInt = parseInt(opponentId, 10);
-
-        if (opponentId !== undefined && typeof opponentIdInt !== 'number') {
-          throw new Error(`Could not parse id of ${opponentId}`);
-        }
-
-        return {isVictory, ...scores, opponentName, opponentId: opponentIdInt};
-      })
-      .map((result) => {
-        const ourScore = result.first > result.second && result.isVictory ? result.first : result.second;
-        const theirScore = result.first > result.second && !result.isVictory ? result.first : result.second;
-
-        return {
-          ...result,
-          ourScore,
-          theirScore
-        };
-      })
-      .filter((result) => {
-        return !!allTeams.find((team) => team.id === result.opponentId);
-      })
-      .reduce(
-        (soFar, result) => {
+      const scores = resultRows
+        .map((resultRow) => {
           return {
-            tournamentTeamsPlayed: soFar.tournamentTeamsPlayed + 1,
-            tournamentTeamsDefeated: result.isVictory
-              ? soFar.tournamentTeamsDefeated + 1
-              : soFar.tournamentTeamsDefeated,
-            tournamentTeamsPointDifferential:
-              soFar.tournamentTeamsPointDifferential + (result.ourScore - result.theirScore),
-            tournamentTeamsResultsLog: [...soFar.tournamentTeamsResultsLog, result]
+            resultCell: scheduleRoot(resultRow).children('td').toArray()[2],
+            opponentCell: scheduleRoot(resultRow).children('td').toArray()[1]
           };
-        },
-        {
-          tournamentTeamsPlayed: 0,
-          tournamentTeamsDefeated: 0,
-          tournamentTeamsPointDifferential: 0,
-          tournamentTeamsResultsLog: []
-        }
-      );
+        })
+        .map((resultCells) => {
+          const result = scheduleRoot(resultCells.resultCell).text();
+          const isVictory = result.startsWith('W');
+          const scores = result
+            .slice(1)
+            .replace(' OT', '')
+            .split('-')
+            .reduce((soFar, part, index) => {
+              const scoreInt = parseInt(part, 10);
 
-    return {
-      ...team,
-      ...scores
-    };
-  })
+              if (typeof scoreInt !== 'number') {
+                throw new Error(`The value ${part} is not a number`);
+              }
+
+              return {...soFar, ...(index === 0 ? {first: scoreInt} : {second: scoreInt})};
+            }, {});
+
+          const opponentLink = scheduleRoot(resultCells.opponentCell).find('a').last();
+          const opponentName = opponentLink.text().trim();
+          const opponentHref = opponentLink.attr('href');
+          const opponentId = opponentHref?.split('/')?.slice(-2, -1).pop();
+
+          const opponentIdInt = parseInt(opponentId, 10);
+
+          if (opponentId !== undefined && typeof opponentIdInt !== 'number') {
+            throw new Error(`Could not parse id of ${opponentId}`);
+          }
+
+          return {isVictory, ...scores, opponentName, opponentId: opponentIdInt};
+        })
+        .map((result) => {
+          const ourScore = result.first > result.second && result.isVictory ? result.first : result.second;
+          const theirScore = result.first > result.second && !result.isVictory ? result.first : result.second;
+
+          return {
+            ...result,
+            ourScore,
+            theirScore
+          };
+        })
+        .filter((result) => {
+          return !!allTeams.find((team) => team.id === result.opponentId);
+        })
+        .reduce(
+          (soFar, result) => {
+            return {
+              tournamentTeamsPlayed: soFar.tournamentTeamsPlayed + 1,
+              tournamentTeamsDefeated: result.isVictory
+                ? soFar.tournamentTeamsDefeated + 1
+                : soFar.tournamentTeamsDefeated,
+              tournamentTeamsPointDifferential:
+                soFar.tournamentTeamsPointDifferential + (result.ourScore - result.theirScore),
+              tournamentTeamsResultsLog: [...soFar.tournamentTeamsResultsLog, result]
+            };
+          },
+          {
+            tournamentTeamsPlayed: 0,
+            tournamentTeamsDefeated: 0,
+            tournamentTeamsPointDifferential: 0,
+            tournamentTeamsResultsLog: []
+          }
+        );
+
+      return {
+        ...team,
+        ...scores
+      };
+    })
 );
 
-console.log(JSON.stringify(allTeamsWithStatus));
+console.log(
+  JSON.stringify(
+    matchupInfo.map((matchup) => {
+      return matchup.map((team) => {
+        const teamWithStats = allTeamsWithStatus.find((allTeamWithStats) => allTeamWithStats.id === team.id);
+
+        return teamWithStats || team;
+      });
+    })
+  )
+);
 /*
   .map((el) => {
     return el.attribs['href'];
